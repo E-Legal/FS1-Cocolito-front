95e5a93aee85c5a56aaedf48e0183e4ef4dce66f
Eliott <legal.eliott@gmail.com>
2020-09-10 10:47:48 +0200

    add build

---
+++ build/precache-manifest.5312a49d2b56feef20adc140f14d8352.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603dd3be725ba911cd3ed07e7c1e5861",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f800d9b5cdad4a320e20",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d746782604ad7908fb01",
+    "url": "/static/css/main.2bee839f.chunk.css"
+  },
+  {
+    "revision": "f800d9b5cdad4a320e20",
+    "url": "/static/js/2.f8886edb.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/static/js/2.f8886edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d746782604ad7908fb01",
+    "url": "/static/js/main.d4c75539.chunk.js"
+  },
+  {
+    "revision": "e618b030a70b9a19a622",
+    "url": "/static/js/runtime-main.f95cae8f.js"
+  }
+]);
