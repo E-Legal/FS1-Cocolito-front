18722d15f7ab144320247e603e3110e080cc4a21
Eliott <legal.eliott@gmail.com>
2020-09-17 09:41:17 +0200

    Chatroom update

--- src/components/chatroom/Chatroom.js
+++ src/components/chatroom/Chatroom.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import TextField from '@material-ui/core/TextField';
+import '../../css/Chatroom.css';
 import io from 'socket.io-client';
 
 const socket = io.connect('http://localhost:4000');
@@ -28,12 +29,12 @@ const Chatroom = (props) => {
   };
 
   const renderChat = () => chat.map(({ name, message }, index) => (
-    <div key={index}>
-      <p>
-        {name}
-        :
-        <span>{message}</span>
-      </p>
+    <div key={index} className="card z-depth-0 project-summary">
+      <div className="card-content green-text text-darken-3">
+        <pre className="green-text">
+          {name} : <span>{message}</span>
+        </pre>
+      </div>
     </div>
   ));
 
@@ -43,27 +44,27 @@ const Chatroom = (props) => {
   return (
     <div className="container">
       <div className="row">
-      <div className="col s12 m5 offset-m1">
-        <form onSubmit={onMessageSubmit}>
-          <div className="input-field">
-            <TextField
+        <div className="col s12 m5 offset-m1">
+          <form onSubmit={onMessageSubmit}>
+            <div className="input-field">
+              <TextField
                 name="message"
                 onChange={(e) => onTextChange(e)}
                 value={state.message}
                 multiline
                 rowsMax={20}
                 id="filled-multiline-flexible"
-                variant="outlined"
+                variant="filled"
                 label="Message"
-            />
-          </div>
-          <button>Send Message</button>
-        </form>
-      </div>
-      <div className="col s12 m6">
-        <h2>Chat Log :</h2>
-        {renderChat()}
-      </div>
+              />
+            </div>
+            <button className="btn grey lighten-1 z-depth-0" >Send Message</button>
+          </form>
+        </div>
+        <div className="col s12 m6 chatroom_page">
+          <h2>CHAT LOG :</h2>
+          {renderChat()}
+        </div>
       </div>
     </div>
   );
